9835b7750aaf00ce49fb8b908bef40be99bd7487
CaptainEddy <aishwarydangre@gmail.com>
2021-11-12 08:54:33 +0530

    new prject

---
+++ src/main/java/com/cob/_2021_11_12_Git_Maven_Project/App.java
@@ -0,0 +1,13 @@
+package com.cob._2021_11_12_Git_Maven_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/cob/_2021_11_12_Git_Maven_Project/AppTest.java
@@ -0,0 +1,38 @@
+package com.cob._2021_11_12_Git_Maven_Project;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
