24f64b32af02a9a3b9706d7b793e299678afd1f9
CaptainEddy <aishwarydangre@gmail.com>
2021-11-12 17:22:48 +0530

    modified demo

--- src/main/java/com/cob/_2021_11_12_Git_Maven_Project/Demo.java
+++ src/main/java/com/cob/_2021_11_12_Git_Maven_Project/Demo.java
@@ -3,8 +3,7 @@ package com.cob._2021_11_12_Git_Maven_Project;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Welcome Captain!");
 	}
 
 }
